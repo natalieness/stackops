9050a67f40cb9a460ab26b5cadbd7ed3e01f4954
natalieness <natalie.ness@outlook.com>
2026-02-20 14:18:11 +0000

    corrected typo

--- useful_gs_utils.py
+++ useful_gs_utils.py
@@ -1,2 +1,2 @@
 # to check what's in the bucket :) 
-# gsutil -o "Credentials:gs_service_key_file=$HOME/.cloudvolume/google-secret.json"   ls gs://path/to/bucket/dataset/layer/sample1/image
+# gsutil -o "Credentials:gs_service_key_file=$HOME/.cloudvolume/secrets/google-secret.json"   ls gs://path/to/bucket/dataset/layer/sample1/image
